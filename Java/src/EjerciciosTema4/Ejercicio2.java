9c4da75e987930543c9ff6f8c50dec792bd9d47a
Esteban1876 <juanes.e1876@gmail.com]>
2022-08-23 15:46:38 -0500

    Ejercicio dos tema 4 de iniciación a la programación completado

---
+++ Java/src/EjerciciosTema4/Ejercicio2.java
@@ -0,0 +1,64 @@
+package EjerciciosTema4;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        int numeroIf = 1;
+        int numeroWhile = 0;
+        int numeroDoWhile = 0;
+        int numeroFor = 0;
+        String estacion = "verano";
+
+        //condicional
+        if (numeroIf <0){
+            System.out.println("El numero es negativo");
+        } else if (numeroIf >0) {
+            System.out.println("El numero es positivo");
+        } else {
+            System.out.println("El numero es cero");
+        }
+
+        //while
+        while (numeroWhile <= 3){
+            System.out.println("Ciclo while");
+            System.out.println(": " + numeroWhile);
+            numeroWhile++;
+        }
+
+        //do while
+        do {
+            System.out.println("Ciclo do while");
+            System.out.println(": " + numeroDoWhile);
+            numeroDoWhile++;
+            System.out.println("Ahora es: " + numeroDoWhile);
+        } while (numeroDoWhile==1);
+
+        //for
+        for (int i=0; i<=3; i++){
+            System.out.println("Bucle for");
+            System.out.println(": " + i);
+        }
+
+        //switch
+        switch (estacion){
+            case "verano":
+                System.out.println("La estacion actual es verano");
+                break;
+            case "primavera":
+                System.out.println("La estacion actual es primavera");
+                break;
+            case "invierno":
+                System.out.println("La estacion actual es invierno");
+                break;
+            case "otono":
+                System.out.println("La estacion actual es otono");
+                break;
+
+            default:
+                System.out.println("Esta estacion no existe");
+        }
+    }
+
+
+
+}
