04ada28f3d30644580640ecb164c4f3612d6350a
Esteban1876 <juanes.e1876@gmail.com]>
2022-08-23 15:50:56 -0500

    correccion del ciclo do while

--- Java/src/EjerciciosTema4/Ejercicio2.java
+++ Java/src/EjerciciosTema4/Ejercicio2.java
@@ -5,7 +5,7 @@ public class Ejercicio2 {
     public static void main(String[] args) {
         int numeroIf = 1;
         int numeroWhile = 0;
-        int numeroDoWhile = 0;
+        int numeroDoWhile = 1;
         int numeroFor = 0;
         String estacion = "verano";
 
